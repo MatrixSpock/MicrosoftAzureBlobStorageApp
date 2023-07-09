f38630e8d76f8c44c6b7c419b50557f65201811a
Jose Ibarra <jibarra8917@gmail.com>
2023-07-09 13:28:52 -0600

    Setting up Blob upload functionality in method

---
+++ WiredBrainCoffee.Storage/CoffeeVideoStorage.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.Storage
+{
+    public class CoffeeVideoStorage : ICoffeeVideoStorage
+    {
+        private readonly string _containerNameVideos = "coffeevideos";
+        private readonly string _connectionString;
+
+        public CoffeeVideoStorage(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task UploadVideoAsync(byte[] videoByteArray, string blobName)
+        {
+            var cloudStorageAccount = CloudStorageAccount.Parse(_connectionString);
+
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            var cloudBlobContainer = cloudBlobClient.GetContainerReference(_containerNameVideos);
+            //Blob grants public read access to our blob
+            await cloudBlobContainer.CreateIfNotExistsAsync(
+                BlobContainerPublicAccessType.Blob, null, null);
+
+            var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(blobName);
+
+            await cloudBlockBlob.UploadFromByteArrayAsync(videoByteArray, 0, videoByteArray.Length);
+
+        }
+
+        public async Task<bool> CheckIfBlobExistsAsync(string blobName)
+        {
+            // TODO: Check if the blob exists in Blob Storage
+            return false;
+        }
+    }
+}
