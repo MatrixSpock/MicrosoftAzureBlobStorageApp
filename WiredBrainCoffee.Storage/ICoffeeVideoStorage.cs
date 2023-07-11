a4bbbc5daa6a037328e6f33c0f6dddadd2789a2e
Jose Ibarra <jibarra8917@gmail.com>
2023-07-10 22:40:03 -0600

    Module 05 - Playing video in the UWP app

--- WiredBrainCoffee.Storage/ICoffeeVideoStorage.cs
+++ WiredBrainCoffee.Storage/ICoffeeVideoStorage.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Azure.Storage.Blob;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace WiredBrainCoffee.Storage
@@ -10,5 +11,8 @@ namespace WiredBrainCoffee.Storage
     Task<bool> CheckIfBlobExistsAsync(string blobName);
 
     Task<IEnumerable<CloudBlockBlob>> ListVideoBlobsAsync(string prefix = null);
+
+    Task DownloadVideoAsync(CloudBlockBlob cloudBlockBlob, Stream targetStream);
+    Task DeleteVideoAsync(CloudBlockBlob cloudBlockBlob);
   }
 }
